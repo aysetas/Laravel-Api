4350b0eb1e0998dcd2e3c5bc219388880300b013
Ayşe TAŞ <41993475+aysetas@users.noreply.github.com>
2021-04-26 00:10:29 +0300

    product Api read(get)'

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $product_name=$this->faker->sentence(3);
+        return [
+            'name' => $product_name,
+            'slug'=>Str::slug($product_name),
+            'desc'=>$this->faker->paragraph(5),
+            'price'=>mt_rand(10,100)/10
+        ];
+    }
+}
